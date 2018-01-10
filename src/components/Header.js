7f3284733140d76c1ab494e8203d36f1b856fadb
Erik Nguyen <namkhanh164@gmail.com>
2018-01-10 16:26:06 +0800

    wired up react-router.Link to navigate through components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-light">
         <div className="container">
-          <a href="" className="navbar-brand">
+
+          <Link to="/" className="navbar=brand">
             {this.props.appName.toLowerCase()}
-          </a>
+          </Link>
+
+          <ul className="nav navbar-nav pull-xs-right">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="login" className="nav-link">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+
         </div>
       </nav>
     );
